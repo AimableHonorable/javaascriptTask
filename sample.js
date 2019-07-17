bd71c70e76d2416a4bd22dfbe411a84b4b3e1030
Aimable <aimatwiri@gmail.com>
2019-07-17 15:52:25 +0200

    Corrected

--- sample.js
+++ sample.js
@@ -1,33 +1,31 @@
 $(document).ready(function(){
-    var subject_points;
-    var sum;
-    var average;
-    function score_indicate(){
+    let subjectPoints;
+    let average;
+    function scoreIndicate(){
       // By making such a description, in the variable called subject_points
       // You can create an array of [language score, English score, math score, science score, society score].
-      subject_points = [Number($('#national_language').val()),
+      subjectPoints = [Number($('#national_language').val()),
                             Number($('#english').val()),
                             Number($('#mathematics').val()),
                             Number($('#science').val()),
                             Number($('#society').val())
                             ];
-  
+                            
       // Furthermore, by making such a description, the total point is output to the right part: "total point:"
-      sum = subject_points[0];
-      sum = sum + subject_points[1];
-      sum = sum + subject_points[2];
-      sum = sum + subject_points[3];
-      sum = sum + subject_points[4];
+      let sum=0;
+      subjectPoints.forEach(function(Number){ 
+        sum += Number;
+      });
       $("#sum_indicate").text(sum);
       // write the process to output the average point referring to the above here
-      average = sum / subject_points.length;
+      average = sum / subjectPoints.length;
       $("#avarage_indicate").text(average);
-      return subject_points;
+      return subjectPoints;
     };
   
-    function get_achievement(){
+    function getAchievement(){
       //rank values ("A" if the average score is 80 or more, "B" if it is 60 or more, "C" if it is 40 or more, "D" if it is less than 40)
-      if(average >= 80){
+      if(average >= 80 && average<=100){
        $('#evaluation').text("A");
       }
       else if(average >= 60 && average < 80){
@@ -42,39 +40,40 @@ $(document).ready(function(){
       return $("#evaluation").text();
     };
   
-    function get_pass_or_failure(){
+    function getPassOrFailure(){
       //"pass" if all subjects have 60 or more points, and a character string of "fail" if there is at least one subject with less than 60 points.
-      let subject_points = score_indicate();
+      let subjectPoints = scoreIndicate();
       
       $("#judge").text("Passed");
-          for(let i=0; i<subject_points.length; i++){
-              if(subject_points[i]<60){
-                $("#judge").text("Failed");
-              }
+        for(let i=0; i<subjectPoints.length; i++){
+          if(subjectPoints[i]<60){
+            $("#judge").text("Failed");
           }
+        }
         return $("#judge").text();
     };
   
     function judgement(){
-      let achievement = get_achievement();
-      let pass_fail = get_pass_or_failure();
+      let achievement = getAchievement();
+      let pass_fail = getPassOrFailure();
       // By writing the following, if you click the button of  as “Your"final judge", "Your grade is (the value of" rank "is put here). A process is implemented in which a light blue balloon with the text “(The value of“ judgment ”) is is output.
       $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">Your grade is ${achievement}. You ${pass_fail}!!!</label>`);
     };
   
     $('#national_language, #english, #mathematics, #science, #society').change(function() {
-      score_indicate();
+      scoreIndicate();
     });
   
     $('#btn-evaluation').click(function() {
-      get_achievement();
+      getAchievement();
     });
   
     $('#btn-judge').click(function() {
-      get_pass_or_failure();
+      getPassOrFailure();
     });
   
     $('#btn-declaration').click(function() {
+      $('#declaration').text("");
       judgement();
     });
   });
